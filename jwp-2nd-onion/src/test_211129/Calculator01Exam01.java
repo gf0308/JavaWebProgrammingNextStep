d69871f5420889776755bbf8abd1776535da071d
gf0308 <kjbk0308@gmail.com>
2021-11-30 22:05:44 +0900

    커밋

--- jwp-2nd-onion/src/test_211129/Calculator01Exam01.java
+++ jwp-2nd-onion/src/test_211129/Calculator01Exam01.java
@@ -1,45 +1,45 @@
 package test_211129;
 
 /*
- * - Ŀ���ұ����ڴ� ���Թ��ڿ��� �տ� �´�
- * - �Ϲݱ�����, Ŀ���ұ����ڷ� ���еǾ� ǥ���� ���ڹ��ڿ��� �ݵ�� ���ڰ��� �����ڷθ� �̷���� ä�� ���԰����� ����´�, �������� ���ڵ�� �������� �ʴ´�
+ * 다음 3가지 원칙을 고려해서 리팩토링 하기
+ * - 1) 한 메서드는 하나의 책임만 갖기
+ * - 2) 인덴트(indent) 의 깊이(depth)는 2이상되지 않도록 하기
+ * - 3) else 문은 사용하지 않기
+ *
  * */
 
 public class Calculator01Exam01 {
 
-	// ���� ���� �޼���
+	// 문자열숫자 총합 계산 수행 메서드
 	public int calculateStrNumSum(String param) {
 		String[] arrs = null;
 
-		if(param.contains("//") && param.contains("\n")) { // Ŀ���� �����ڷ� ���е� ���
+		if(param.contains("//") && param.contains("\n")) {
 			arrs = getNumsByCustomDivider(param);
 		}
 
-		if(param.contains(",") || param.contains(":")) { // �⺻ �����ڷ� ���е� ���
+		if(param.contains(",") || param.contains(":")) {
 			arrs = getNumsByBasicDivider(param);
 		}
 
-		isThereNegativeNumber(arrs); // ��� �� ������ ���� �ִ��� Ȯ�� : true��� RuntimeException ��ȯ
+		isThereNegativeNumber(arrs);
 
-		return sumNumbers(arrs); // ��ҵ��� �� ���Ѵ�
+		return sumNumbers(arrs);
 	}
 
 
-	// Ŀ���ұ����ڷ� ���ڰ� �迭 ���
 	public String[] getNumsByCustomDivider(String param) {
 		int leftMarkIndex = param.lastIndexOf("//");
 		int rightMarkIndex = param.lastIndexOf("\n");
 		String customDivider = param.substring(leftMarkIndex+2, rightMarkIndex);
-		String cleanedParam = param.substring(rightMarkIndex+1); // Ŀ���ұ����ڸ� �������� ���ø��ϱ� ��, ���Թ��ڿ������� �����ںκ��� �������ֱ�
+		String cleanedParam = param.substring(rightMarkIndex+1);
 		return cleanedParam.split(customDivider);
 	}
 
-	// ��б����ڷ� ���ڰ� �迭 ���
 	public String[] getNumsByBasicDivider(String param) {
 		return param.split(",|:");
 	}
 
-	// �������� üũ�ϱ�
 	public void isThereNegativeNumber(String[] strNums) {
 		for(String strNum : strNums) {
 			if(Integer.parseInt(strNum) < 0) {
@@ -48,7 +48,6 @@ public class Calculator01Exam01 {
 		}
 	}
 
-	// ���ڰ� ���� ���
 	public int sumNumbers(String[] strNums) {
 		int sum = 0;
 		for(String strNum : strNums) {
